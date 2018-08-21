2ceae59cea8e8a94c3fa5b4cc1b4025d08e490b8
jang <dragoalie@gmail.com>
2018-08-21 09:28:56 +0900

    Add arrow function

--- graffiti/cons_car_cdr.js
+++ graffiti/cons_car_cdr.js
@@ -3,18 +3,36 @@ function cons(a,b) {
   return function(pair) {
     return pair(a, b);
   };
-}
+};
 
 function car(pair) {
   return pair(function(a, b){
     return a;
   });
-}
+};
 
 function cdr(pair) {
   return pair(function(a, b) {
     return b;
   });
-}
+};
 
-cons(1, cons(2, 3));
+
+var cons = (a, b) => {
+  return (pair) => pair(a, b)
+};
+
+var car = (pair) => {
+  return pair((a, b) => a);
+};
+
+var cdr = (pair) => {
+  return pair((a, b) => b);
+};
+
+
+// get 'a'
+console.log(
+  car(cdr(cons(cons(99, 100), cons('a', 'b'))))
+);
+
